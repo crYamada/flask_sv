da8b61bcc45c3212dbdf0f98849c4fd045731de8
Yamada <n.yamada@creist.co.jp>
2021-10-21 10:43:33 +0900

    関数名の変更

--- backend/src/restapi/project3_app.py
+++ backend/src/restapi/project3_app.py
@@ -7,6 +7,6 @@ system_account_id = 999
 project3_bp = Blueprint('project3_app', __name__, url_prefix='/api/project3')
 
 @project3_bp.route('/get/<id>', methods=['GET'])
-def getAccount(id):
+def getProject(id):
     account_json = ProjectApi.getById(id, system_account_id)
     return jsonify(account_json)
