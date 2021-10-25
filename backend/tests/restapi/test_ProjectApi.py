002b4b215dc3d7e0c6afad38737b023d9a2b5299
Yamada <n.yamada@creist.co.jp>
2021-10-25 18:03:57 +0900

    update_for_lock APIのテストコードの追加

--- backend/tests/restapi/test_ProjectApi.py
+++ backend/tests/restapi/test_ProjectApi.py
@@ -159,6 +159,93 @@ def test_project_update():
         'project_name': 'update_project_modified',
     }
     result = Project.search(search_query, 999)
-    assert result[0].created_by == project['creater_id']
-    assert result[0].updated_by == operation_account_id
+    assert result[0].creater_id == project['creater_id']
+    assert result[0].updater_id == operation_account_id
+    assert result[0].status == payload['status']
+
+
+def test_project_update_with_lock():
+    """
+    """
+    project = {
+        'project_name': 'update_project_lock',
+        'description': 'test of update_for_lock api',
+        'status': 0,
+        'creater_id': 701,
+        'created_at': '2021-01-01 00:00:00',
+        'updater_id': 701,
+        'updated_at': '2021-12-31 00:00:00'
+    }
+
+    operation_account_id = 998
+    # create
+    Project.create(project, 999) == True
+
+    search_query = {
+        'project_name': 'update_project_lock',
+        'status': 0,
+        'creater_id': 701
+    }
+    result = Project.search(search_query, 999)
+    assert result[0].project_name == project['project_name']
+    project_id = result[0].id
+
+    payload = {
+        "id": project_id,
+        "operation_account_id": operation_account_id
+    }
+
+    # APIから検索しロックをする
+    url = f"http://localhost:5000/api/project3/lock"
+    headers = {'Accept-Encoding': 'identity, deflate, compress, gzip',
+               'Accept': '*/*', 'User-Agent': 'flask_sv/0.0.1',
+               'Content-type': 'application/json; charset=utf-8',
+               }
+    response = requests.post(url, headers=headers, json=payload)
+    print(f"lock:{response}")
+    assert response.status_code == 200
+
+    data = json.loads(response.text)
+    print(f"data={data}")
+    assert data['body']['id'] == project_id
+    assert data['body']['project_name'] == project['project_name']
+    assert data['body']['description'] == project['description']
+    assert data['body']['status'] == project['status']
+    assert data['body']['creater_id'] == project['creater_id']
+    assert data['body']['updater_id'] == project['updater_id']
+    assert data['status']['code'] == "I0001"
+    assert data['status']['message'] == ""
+
+    # TODO ロックしたレコードを更新しようとするとロックされることを数秒間応答がないことで確認する
+
+    payload = {
+        'id': project_id,
+        'project_name': 'update_project_lock2',
+        'status': 2,
+        'operation_account_id': operation_account_id
+    }
+
+    # ロックしたレコードを更新する
+    url = f"http://localhost:5000/api/project3/update_for_lock"
+    headers = {'Accept-Encoding': 'identity, deflate, compress, gzip',
+               'Accept': '*/*', 'User-Agent': 'flask_sv/0.0.1',
+               'Content-type': 'application/json; charset=utf-8',
+               }
+    response = requests.post(url, headers=headers, json=payload)
+
+    # HTTP Statusコードが200であること
+    print(f"update_lock:{response}")
+    assert response.status_code == 200
+    data = json.loads(response.text)
+    assert data['body'] == ""
+    assert data['status']['code'] == "I0001"
+    assert data['status']['message'] == "Updating project was successfully."
+
+    search_query = {
+        'project_name': 'update_account_lock2',
+    }
+    result = Project.search(search_query, 999)
+    assert result[0].project_name == payload['project_name']
+    assert result[0].creater_id == project['creater_id']
+    assert result[0].updater_id == operation_account_id
     assert result[0].status == payload['status']
