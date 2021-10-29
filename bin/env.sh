7dd4a10df36185a4d2c59ac73a8edb39497314fd
Yamada <n.yamada@creist.co.jp>
2021-10-29 17:22:58 +0900

    DOCKER_HOMEのPathの更新

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,5 @@
 
-DOCKER_HOME=C:/Users/psuser/CREiST/Projects/docker_mysql_flask_sv/docker-mysql
+DOCKER_HOME=C:/Users/psuser/CREiST/Projects/in-house_study_2021/docker_mysql_flask_sv/docker-mysql
 
 IFS=$'\n'
 for line in `cat $DOCKER_HOME/.env`
